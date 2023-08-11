1e72964b6e47797642f475e249782c4058923906
Karol Saputa <karol.saputa@gmail.com>
2023-08-11 14:10:53 +0200

    feat(config): add pip-installable yaml config

---
+++ python_template/config.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import pkg_resources
+import yaml
+
+
+def get_config(config_path: Path = None) -> 'Config':
+    if config_path is None:
+        config_path = pkg_resources.resource_filename(
+            'python_template',
+            'config.yaml'
+        )
+    elif config_path.exists():
+        pass
+    else:
+        raise FileNotFoundError(f'Config file {config_path} not found')
+
+    with open(config_path, 'r') as f:
+        cfg = yaml.safe_load(f)
+    return Config(**cfg)
+
+
+@dataclass
+class YamlConfig:
+    is_working: bool = False
+
+
+@dataclass
+class Config:
+    yaml_config: YamlConfig = field(default_factory=YamlConfig)
+
+    def __post_init__(self):
+        self.yaml_config = YamlConfig(**self.yaml_config)
