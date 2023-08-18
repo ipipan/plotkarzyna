5893faa702153f4e52f26621748c22f7b80a9d38
Karol Saputa <ksaputa@gputrain.dariah.ipipan.waw.pl>
2023-08-18 16:06:52 +0200

    feat: baseline model training

--- plotkarzyna/dataset.py
+++ plotkarzyna/dataset.py
@@ -4,6 +4,71 @@ import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
+id2label = {
+    0: "O",
+    1: "B",
+    2: "I",
+    3: "E",
+    4: "S"
+}
+label2id = {
+    "O": 0,
+    "B": 1,
+    "I": 2,
+    "E": 3,
+    "S": 4,
+}
+
+
+def add_bioes(texts):
+    for text in texts:
+        annotation = ['O'] * len(text.segments)
+        mentions = sorted(
+            [mention for cluster in text.clusters for mention in cluster], key=lambda x: x[0])
+        for mention in mentions:
+            start, end = mention
+            if start == end:
+                annotation[start] = 'S'
+            else:
+                annotation[start] = 'B'
+                annotation[end] = 'E'
+            for ind in range(start, end):
+                if annotation[ind] == 'O':
+                    annotation[ind] = 'I'
+        annotation = [label2id[ann] for ann in annotation]
+        text.mention_annotation = annotation
+
+
+def tokenize_and_align_labels(text, tokenizer):
+    tokenized_inputs = tokenizer(
+        text["tokens"],
+        is_split_into_words=True,
+        truncation=True,
+        padding='max_length',
+        # return_tensors='pt',
+    )
+
+    labels = []
+    for i, label in enumerate(text[f'labels']):
+        # Map tokens to their respective word.
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:  # Set the special tokens to -100.
+            if word_idx is None:
+                label_ids.append(-100)
+            # Only label the first token of a given word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(label[word_idx])
+            else:
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        label_ids = label_ids[:len(word_ids)]
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
 
 class MentionDS(Dataset):
     def __init__(
@@ -14,13 +79,30 @@ class MentionDS(Dataset):
     ):
         self.texts = texts
         self.tokenizer = tokenizer
-        self.force_reload=force_reload
-        self.length = sum(len(text.segments) for text in self.texts)
+        self.force_reload = force_reload
+        # self.length = sum(len(text.segments) for text in self.texts)
+        add_bioes(self.texts)
+        self.tokenized = [
+            tokenize_and_align_labels(
+            {
+                'tokens': [text.segments],
+                'labels': [text.mention_annotation],
+            }, self.tokenizer
+        ) for text in self.texts]
+        for text in self.tokenized:
+            text['input_ids'] = text['input_ids'][0]
+            text['token_type_ids'] = text['token_type_ids'][0]
+            text['attention_mask'] = text['attention_mask'][0]
+            text['labels'] = text['labels'][0]
+
+        self.labels = [tokenized['labels'][0] for tokenized in self.tokenized]
+        # dict_keys(['input_ids', 'token_type_ids', 'attention_mask'])
 
     def __len__(self):
-        return self.length
-    
+        return len(self.texts)
+
     def __getitem__(self, idx):
-        path = self.all_paths[idx]
-        tokenized_data_point = self.cached_load(path)
-        return tokenized_data_point
+        # path = self.all_paths[idx]
+        # tokenized_data_point = self.cached_load(path)
+        # return tokenized_data_point
+        return self.tokenized[idx] #, self.labels[idx]
---
+++ train_hh.py
@@ -0,0 +1,180 @@
+from pathlib import Path
+
+from dotenv import dotenv_values, load_dotenv
+import seqeval
+
+import coref_ds
+from coref_ds.text import Text
+from coref_ds.mmax.mmax_doc import MmaxDoc
+from coref_ds.tei.tei_doc import TEIDocument
+
+from plotkarzyna.config import *
+
+
+import evaluate
+
+seqeval = evaluate.load("seqeval")
+
+try:
+    local_config = dotenv_values(".env")
+except FileNotFoundError:
+    local_config = dict()
+load_dotenv()
+n_samples = 100_000_000
+mmax_paths = list(Path(local_config['NKJP_MMAX_ROOT']).glob('**/*.mmax'))[:n_samples]
+tei_train_paths = list(
+    (Path(local_config['PCC_ROOT']) / 'train').iterdir()
+    )[:n_samples]
+
+test_paths = list(
+    (Path(local_config['PCC_ROOT']) / 'test').iterdir()
+    )[:n_samples]
+
+mmax_docs = []
+for p in mmax_paths:
+    try: 
+        m = MmaxDoc.from_file(p)
+        mmax_docs.append(m)
+    except Exception as e:
+        print(e)
+
+
+tei_docs = []
+for p in tei_train_paths:
+    try: 
+        m = TEIDocument(p)
+        tei_docs.append(m)
+    except Exception as e:
+        print(e)
+
+
+test_docs = []
+for p in test_paths:
+    try: 
+        m = TEIDocument(p)
+        test_docs.append(m)
+    except Exception as e:
+        print(e)
+
+texts = []
+test_texts = []
+
+for m in tei_docs:
+    texts.append(m.text)
+
+for m in test_docs:
+    test_texts.append(m.text)
+
+from transformers import AutoTokenizer
+from plotkarzyna.dataset import MentionDS
+tokenizer = AutoTokenizer.from_pretrained('allegro/herbert-base-cased')
+
+train_ds = MentionDS(
+    texts,
+    tokenizer
+)
+
+test_ds = MentionDS(
+    test_texts,
+    tokenizer
+)
+
+# for text in train_ds:
+#     assert len(text['input_ids']) == len(text['labels'])
+
+id2label = {
+    0: "O",
+    1: "B",
+    2: "I",
+    3: "E",
+    4: "S"
+}
+label2id = {
+    "O": 0,
+    "B": 1,
+    "I": 2,
+    "E": 3,
+    "S": 4,
+}
+
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer
+
+model = AutoModelForTokenClassification.from_pretrained(
+    "allegro/herbert-large-cased", num_labels=5, id2label=id2label, label2id=label2id
+)
+
+from transformers import DataCollatorForTokenClassification
+
+data_collator = DataCollatorForTokenClassification(
+    tokenizer=tokenizer,
+    max_length=512,
+    padding='max_length',
+    )
+
+import numpy as np
+
+def compute_metrics(p):
+    predictions, labels = p
+    label_list = list(label2id.keys())
+    predictions = np.argmax(predictions, axis=2)
+
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+training_args = TrainingArguments(
+    output_dir=Path(local_config['MODEL_DIR']) / 'herbert-large',
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=20,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_ds,
+    eval_dataset=test_ds,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+from torch.utils.data import DataLoader
+for ind, sample in enumerate(train_ds):
+    # print(sample['input_ids'].shape, len(sample['labels']))
+    if ind > 5:
+        break
+
+dl = DataLoader(
+    train_ds,
+    batch_size=16,
+    shuffle=True,
+    collate_fn=data_collator,
+    )
+for ind, batch in enumerate(dl):
+    print(batch)
+    print(batch['input_ids'].shape, batch['token_type_ids'].shape, batch['attention_mask'].shape, batch['labels'].shape)
+    if ind > 5:
+        break
+
+
+
+trainer.train()
